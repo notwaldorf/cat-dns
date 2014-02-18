ed754c08190dc38bd58d9e802ee884b85482db68
monica <notwaldorf@gmail.com>
2014-02-17 20:24:15 -0500

    some refactoring.

--- cat-dns.js
+++ cat-dns.js
@@ -16,8 +16,64 @@ catServer.listen(80);
 var dnsServer = dgram.createSocket('udp4');
 dnsServer.bind(53, 'localhost');
 
+
+dnsServer.on('message', function (msg, rinfo) {
+  var start = new Date().getTime();
+  var query = parseQuestion(new BitArray.fromBuffer(msg));
+  var queryEnd = new Date().getTime();
+
+  var answer = createCatAnswer(query);
+  var answerEnd = new Date().getTime();
+  
+  var buffer = answer.toBuffer();
+  var bufferEnd = new Date().getTime();
+
+  console.log("parse question: %s ms, assemble answer %s ms, convert buffer %s ms", 
+      queryEnd - start, answerEnd - queryEnd, bufferEnd - answerEnd);
+
+  dnsServer.send(buffer, 0, buffer.length, rinfo.port, rinfo.address, function (err, sent) {
+    console.log("and you (%s:%s) get a cat after %s ms: ", 
+       rinfo.address, rinfo.port, (new Date().getTime() - bufferEnd));
+  });
+});
+
+dnsServer.addListener('error', function (e) {
+  console.log("Oh no, cat error", e);
+  throw e;
+});
+
+function parseQuestion(msg) {
+  var query = new DNSMessage();
+
+  var startBit = query.assemble(query.header, DNSSpec.header, msg, 0);
+
+  // Calculate the length of the qname field, as it isn't constant.
+  var qnameLength = msg.length - startBit - 2 * 16;
+  DNSSpec.question[0].bits = qnameLength;
+  query.assemble(query.question, DNSSpec.question, msg, startBit);
+
+  return query;
+}
+
+function createCatAnswer(query) {
+  var cat = new DNSMessage();
+  cat.header = query.header;
+  cat.question = query.question;
+  cat.answer.qname = query.question.qname;
+  cat.transmogrifyIntoAnswer();
+
+  var fakeCat = ip.toLong("127.0.0.1");
+  cat.answer.rdata = reverseString(new BitArray.from32Integer(fakeCat).toString());
+  return cat;
+}
+
+/*
+ * DNS Message
+ */
+
 // Fields and their bit sizes in the different message sections, as defined by the DNS spec.
-var headerSection = [
+var DNSSpec = {
+ header: [
   {name:"id", bits: 16},
   {name:"qr", bits: 1},
   {name:"opcode", bits: 4},
@@ -30,40 +86,19 @@ var headerSection = [
   {name:"qd_count", bits: 16},
   {name:"an_count", bits: 16},
   {name:"ns_count", bits: 16},
-  {name:"ar_count", bits: 16}
-];
-
-var questionSection = [
+  {name:"ar_count", bits: 16}],
+question: [
   {name:"qname", bits: -1},  // 32 bits from the end :(
   {name:"qtype", bits: 16},
-  {name:"qclass", bits: 16} 
-];
-
-var answerSection = [
+  {name:"qclass", bits: 16}],
+answer: [
   {name:"qname", bits: -1},  // Same as the question qname
   {name:"qtype", bits: 16},
   {name:"qclass", bits: 16},
   {name:"ttl", bits: 32},
   {name:"rlength", bits:16},
-  {name:"rdata", bits: 32},
-];
-
-dnsServer.on('message', function (msg, rinfo) {
-  var query = parseQuestion(new BitArray.fromBuffer(msg));
-  console.log(query);
-  var cat = createCatAnswer(query);
-  console.log(cat);
-  var answer = cat.toBuffer();
-
-  dnsServer.send(answer, 0, answer.length, rinfo.port, rinfo.address, function (err, sent) {
-    console.log("and you get a cat: ", rinfo.port, rinfo.address);
-  });
-});
-
-dnsServer.addListener('error', function (e) {
-  console.log("Oh no, cat error", e);
-  throw e;
-});
+  {name:"rdata", bits: 32}]
+};
 
 function DNSMessage() {
   this.header = {};
@@ -72,80 +107,51 @@ function DNSMessage() {
   this.toBuffer = function() {
     var giantBinaryString = "";
 
-    for (var i = 0; i < headerSection.length; i++)
-      giantBinaryString += this.header[headerSection[i].name];
+    for (var i = 0; i < DNSSpec.header.length; i++)
+      giantBinaryString += this.header[DNSSpec.header[i].name];
   
-    for (var i = 0; i < answerSection.length; i++) 
-      giantBinaryString += this.answer[answerSection[i].name];
-
-    console.log(giantBinaryString);
+    for (var i = 0; i < DNSSpec.answer.length; i++) 
+      giantBinaryString += this.answer[DNSSpec.answer[i].name];
 
     return getBinaryStringAsBuffer(giantBinaryString);
   }
-}
 
-function parseQuestion(msg) {
-  var query = new DNSMessage();
-
-  // Header section.
-  var startBit = 0;
-  for (var i = 0; i < headerSection.length; i++) {
-    var bitsPerField = headerSection[i].bits;
-    var field = headerSection[i].name;
-    query.header[field] =  getBitSequenceAsString(msg, startBit, bitsPerField);
-    startBit += bitsPerField;
+  this.assemble = function(section, fieldNames, msg, startBit) {
+    for (var i = 0; i < fieldNames.length; i++) {
+      var bitsPerField = fieldNames[i].bits;
+      var field = fieldNames[i].name;
+      section[field] =  getBitSequenceAsString(msg, startBit, bitsPerField);
+      startBit += bitsPerField;
+    }
+    return startBit;
   }
 
-  // Question Section. 
-  // Calculate the length of the qname field, as it isn't set.
-  var qnameLength = msg.length - startBit - 2 * 16;
-  questionSection[0].bits = qnameLength;
+  this.transmogrifyIntoAnswer = function() {
+    // Hardcoded answer fields.
+    this.header.qr = '1';
+    this.header.aa = '0';
+    this.header.tc = '0';
+    this.header.ra = '0';
+    this.header.rcode = '0000';
+    this.header.an_count = this.header.qd_count;
+    this.header.qd_count = this.header.ns_count;
 
-  for (var i = 0; i < questionSection.length; i++) {
-    var bitsPerField = questionSection[i].bits;
-    var field = questionSection[i].name;
-    query.question[field] = getBitSequenceAsString(msg, startBit, bitsPerField);
-    startBit += bitsPerField;
+    // Surely there's a better way.
+    this.answer.qtype = '0000000000000001'; // A
+    this.answer.qclass = '0000000000000001'; // Internet
+    this.answer.ttl = '00000000000000000000000000000001'; // Seconds to cache the answer for;
+    this.answer.rlength = '0000000000000100'; // 4 bytes long.
   }
-
-  return query;
 }
 
-function createCatAnswer(query) {
-  var cat = new DNSMessage();
-  cat.header = query.header;
-  cat.question = query.question;
+/*
+ * Utilities to make things less suck.
+ */
 
-  // Hardcoded answer fields.
-  cat.header.qr = '1';
-  cat.header.aa = '0';
-  cat.header.tc = '0';
-  cat.header.ra = '0';
-  cat.header.rcode = '0000';
-  cat.header.an_count = cat.header.qd_count;
-  cat.header.qd_count = cat.header.ns_count;
-
-  // Surely there's a better way.
-  cat.answer.qname = query.question.qname;
-  cat.answer.qtype = '0000000000000001'; // A
-  cat.answer.qclass = '0000000000000001'; // Internet
-  cat.answer.ttl = '00000000000000000000000000000001'; // Seconds to cache the answer for;
-  cat.answer.rlength = '0000000000000100'; // 4 bytes long.
-
-  //var l = ip.toLong("173.236.227.244");
-  var l = ip.toLong("127.0.0.1");
-  cat.answer.rdata = reverseString(new BitArray.from32Integer(l).toString());
-  
-  console.log("------ cat!" , cat);
-  return cat;
-}
-
-// Utilities to make things less suck.
 function getBitSequenceAsString(bitArray, startBit, numBits) {
   var s = "";
-  for (var i = 0; i < numBits; i++ ) {
+  for (var i = 0; i < numBits; i++ )
     s += bitArray.get(startBit + i);
-  }
   return s;
 }
 
